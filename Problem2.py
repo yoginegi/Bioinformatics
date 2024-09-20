f95a0af029fae6e6ab4eb822f3f248f5169213b0
yoginegi <69087517+yoginegi@users.noreply.github.com>
2024-09-20 09:18:56 -0500

    Strings and Lists

---
+++ Problem2.py
@@ -0,0 +1,8 @@
+a= 22
+b= 27
+c= 97
+d= 102
+str= "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain."
+
+str = str[a:b+1] + " " + str[c:d+1]
+print(str)
